bad22ef84b4c890b858e543a5bc2f8fe53679a71
shoriful-dev <cmshohelrana50@gmail.com>
2026-06-25 22:15:33 +0600

    fix formatting inconsistencies in useUser hook

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,6 +1,6 @@
 'use client';
-import { isAuthorized } from "@/lib/isAuthorized";
-import { useEffect, useState } from "react";
+import { isAuthorized } from '@/lib/isAuthorized';
+import { useEffect, useState } from 'react';
 
 export const useUser = () => {
   const [email, setEmail] = useState(null);
@@ -11,9 +11,9 @@ export const useUser = () => {
       const user = await isAuthorized();
       setEmail(user?.email);
       setLoading(false);
-    }
+    };
     fetchUser();
   }, []);
 
   return { email, loading };
-}
+};
