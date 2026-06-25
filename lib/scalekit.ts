852d53a94be176575d7077ff630df431bf1fd16a
shoriful-dev <cmshohelrana50@gmail.com>
2026-06-25 22:18:23 +0600

    fix: add missing comma for Scalekit initialization

--- lib/scalekit.ts
+++ lib/scalekit.ts
@@ -3,7 +3,7 @@ import { Scalekit } from '@scalekit-sdk/node';
 const scalekit = new Scalekit(
   process.env.SCALEKIT_ENVIRONMENT_URL!,
   process.env.SCALEKIT_CLIENT_ID!,
-  process.env.SCALEKIT_CLIENT_SECRET!
+  process.env.SCALEKIT_CLIENT_SECRET!,
 );
 
 export default scalekit;
